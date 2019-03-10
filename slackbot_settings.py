a6190fa0997df9a35a962d7fc271cc5e16386610
Kensuke24 <hibis.tori@gmail.com>
2019-03-10 23:22:37 +0900

    'apiを変数に'

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,11 +2,13 @@
 # APIをGithubに載せずに管理する方法を試行錯誤中
 # Herokuを使用する。
 
+import os
+
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-408658747810-572968179798-oMExxUp9A3QHLWPjJHm5YDMx"
+API_TOKEN = os.environ["BOT_API_TOKEN"]
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
-DEFAULT_REPLY = "ちょっと何言っているかわからないです。ちなみに、私は今Herokuにいますよ。"
+DEFAULT_REPLY = "ちょっと何言っているかわからないです。環境変数は連携できましたか？"
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ['plugins']
