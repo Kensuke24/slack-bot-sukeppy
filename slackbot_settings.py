19c36e2d2e22739497e9bea45bbaf9da416aeb97
Kensuke24 <hibis.tori@gmail.com>
2019-03-11 01:01:32 +0900

    settingからAPIの除去

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,9 +3,15 @@
 # Herokuを使用する。
 
 import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 # botアカウントのトークンを指定
-API_TOKEN = os.environ["BOT_API_TOKEN"]
+API_TOKEN = os.environ.get("BOT_API_TOKEN")
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "ちょっと何言っているかわからないです。環境変数は連携できましたか？"
