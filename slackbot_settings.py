0e61c5f51712108f38f41916101471756337f611
Kensuke24 <hibis.tori@gmail.com>
2019-03-10 17:59:09 +0900

    make it better

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,7 +3,7 @@
 # Herokuを使用する。
 
 # botアカウントのトークンを指定
-API_TOKEN = "BOT_API_TOKEN"
+API_TOKEN = "xoxb-408658747810-572968179798-oMExxUp9A3QHLWPjJHm5YDMx"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "ちょっと何言っているかわからないです。ちなみに、私は今Herokuにいますよ。"
