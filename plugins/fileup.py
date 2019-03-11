c6a7631956952542bbe38377f6eba1703b5e34f6
Kensuke24 <hibis.tori@gmail.com>
2019-03-12 01:11:08 +0900

    'commit'

--- plugins/fileup.py
+++ plugins/fileup.py
@@ -1,14 +1,19 @@
 from slacker import Slacker
 
+from slackbot.bot import listen_to
 from slackbot.bot import respond_to
 import slackbot_settings
 
+tiku=['ちく', 'チク','蓄']   # リストをlisten_toに入力してanyで反応させたい（今はできてない）
 
-@respond_to('^うぷ$')
+
+@listen_to('ちく')
+@listen_to('チク')
+@listen_to('蓄')
 def todoist_today_list(message):
     slacker = Slacker(slackbot_settings.API_TOKEN)
-    channel = 'channel'
-    file = 'C:\Users\msrel\OneDrive\デスクトップ\Git\slack-bot-sukeppy\plugins\respond_text\tikuryoku.txt'
+    channel = 'CC1LDH4BH'
+    file = 'plugins/respond_text/tikuryoku.txt'     # ここまでの相対パス
 
     slacker.files.upload(file_=file, channels=channel)
     message.send('ファイルをアップロードしました')
